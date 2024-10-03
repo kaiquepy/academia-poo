37473e12e92579d8092f47a130d32e50b2955319
KaykyNery <kaykynav@gmail.com>
2024-10-03 20:31:56 -0300

    Initial commit

---
+++ Academia-poo/src/main/java/kaykynery/academia/poo/AcademiaPoo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kaykynery.academia.poo;
+
+/**
+ *
+ * @author kayky
+ */
+public class AcademiaPoo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
